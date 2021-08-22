b66251f6c269da35da8b5627770179367c30ef25
Bill Avery <wravery@gmail.com>
2021-08-22 15:09:23 -0700

    Reintroduce callback arg conversion traits

--- src/callback.rs
+++ src/callback.rs
@@ -1,6 +1,6 @@
 use std::sync::mpsc;
 
-use windows::{implement, HRESULT};
+use windows::{implement, Interface, HRESULT};
 
 use crate::{
     pwstr::string_from_pwstr,
@@ -9,58 +9,116 @@ use crate::{
     Windows::{self, Win32::Foundation::PWSTR},
 };
 
+pub trait ClosureArg {
+    type Output: Sized;
+}
+
+pub trait InvokeArg<'a> {
+    type Input: 'a;
+
+    fn convert(input: Self::Input) -> <Self as ClosureArg>::Output
+    where
+        Self: ClosureArg;
+}
+
+impl ClosureArg for HRESULT {
+    type Output = windows::Result<()>;
+}
+
+impl<'a> InvokeArg<'a> for HRESULT {
+    type Input = Self;
+
+    fn convert(input: HRESULT) -> windows::Result<()> {
+        input.ok()
+    }
+}
+
+impl ClosureArg for PWSTR {
+    type Output = String;
+}
+
+impl<'a> InvokeArg<'a> for PWSTR {
+    type Input = Self;
+
+    fn convert(input: PWSTR) -> String {
+        string_from_pwstr(input)
+    }
+}
+
+impl<I: Interface> ClosureArg for Option<I> {
+    type Output = Self;
+}
+
+impl<'a, I: 'a + Interface> InvokeArg<'a> for Option<I> {
+    type Input = &'a Self;
+
+    fn convert(input: &'a Self) -> <Self as ClosureArg>::Output {
+        input.clone()
+    }
+}
+
 /// Generic closure signature for [`CompletedCallback`].
-pub type CompletedClosure<Arg1, Arg2> = Box<dyn FnOnce(Arg1, Arg2) -> ::windows::Result<()>>;
+pub type CompletedClosure<Arg1, Arg2> = Box<
+    dyn FnOnce(<Arg1 as ClosureArg>::Output, <Arg2 as ClosureArg>::Output) -> ::windows::Result<()>,
+>;
 
 /// Generic closure signature for [`EventCallback`].
-pub type EventClosure<Arg1, Arg2> = Box<dyn FnMut(Arg1, Arg2) -> windows::Result<()>>;
+pub type EventClosure<Arg1, Arg2> = Box<
+    dyn FnMut(<Arg1 as ClosureArg>::Output, <Arg2 as ClosureArg>::Output) -> windows::Result<()>,
+>;
 
 #[completed_callback]
 pub struct CreateCoreWebView2EnvironmentCompletedHandler(
     ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler,
     HRESULT,
-    ICoreWebView2Environment,
+    Option<ICoreWebView2Environment>,
 );
 
 #[completed_callback]
 pub struct CreateCoreWebView2ControllerCompletedHandler(
     ICoreWebView2CreateCoreWebView2ControllerCompletedHandler,
     HRESULT,
-    ICoreWebView2Controller,
+    Option<ICoreWebView2Controller>,
 );
 
 #[event_callback]
 pub struct WebMessageReceivedEventHandler(
     ICoreWebView2WebMessageReceivedEventHandler,
-    ICoreWebView2,
-    ICoreWebView2WebMessageReceivedEventArgs,
+    Option<ICoreWebView2>,
+    Option<ICoreWebView2WebMessageReceivedEventArgs>,
 );
 
 #[event_callback]
 pub struct WebResourceRequestedEventHandler(
     ICoreWebView2WebResourceRequestedEventHandler,
-    ICoreWebView2,
-    ICoreWebView2WebResourceRequestedEventArgs,
+    Option<ICoreWebView2>,
+    Option<ICoreWebView2WebResourceRequestedEventArgs>,
 );
 
 #[event_callback]
 pub struct PermissionRequestedEventHandler(
     ICoreWebView2PermissionRequestedEventHandler,
-    ICoreWebView2,
-    ICoreWebView2PermissionRequestedEventArgs,
+    Option<ICoreWebView2>,
+    Option<ICoreWebView2PermissionRequestedEventArgs>,
 );
 
 #[event_callback]
 pub struct NavigationCompletedEventHandler(
     ICoreWebView2NavigationCompletedEventHandler,
-    ICoreWebView2,
-    ICoreWebView2NavigationCompletedEventArgs,
+    Option<ICoreWebView2>,
+    Option<ICoreWebView2NavigationCompletedEventArgs>,
 );
 
-#[string_callback]
+#[completed_callback]
 pub struct AddScriptToExecuteOnDocumentCreatedCompletedHandler(
     ICoreWebView2AddScriptToExecuteOnDocumentCreatedCompletedHandler,
+    HRESULT,
+    PWSTR,
 );
 
-#[string_callback]
-pub struct ExecuteScriptCompletedHandler(ICoreWebView2ExecuteScriptCompletedHandler);
+#[completed_callback]
+pub struct ExecuteScriptCompletedHandler(
+    ICoreWebView2ExecuteScriptCompletedHandler,
+    HRESULT,
+    PWSTR,
+);
